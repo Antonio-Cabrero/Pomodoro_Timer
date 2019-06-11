753d7b70bc0e9a92d5776d9c3568c697b00e0d02
ZealousSloth <49397161+ZealousSloth@users.noreply.github.com>
2019-06-11 23:37:18 +0200

    add seconds countdown
    added a function that counts down seconds properly

--- timer.js
+++ timer.js
@@ -1,19 +1,21 @@
 //Timer Fuction
-function pomodoroTimer () {
-var durationMin = 25;
-var durationSec = 60;
 
-while (durationMin > 0) {
-     a = setInterval(
-    if (durationSec > 0) {
-    durationSec -= 1;
-    console.log(durationSec)},100);
-} else {
-    durationMin -= 1;
-    console.log(durationMin);
-}
-}
+//seonds
+function timerSec() {
+
+    var sec = 5;
+    seconds = setInterval(function () 
+      {  
+        sec = sec - 1;
+        if (sec == 0) {
+            clearInterval(seconds);
+        }
+        console.log(sec); 
+            
+        }, 1000);
+    
+} 
 
 // document.getElementById('minutes').innerHTML= durationMin;
 // document.getElementById('sec').innerHTML= durationSec;
-}
+
