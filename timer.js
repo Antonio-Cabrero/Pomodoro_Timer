910bfa35f79d6345c21b3bad2f937e478451f347
ZealousSloth <49397161+ZealousSloth@users.noreply.github.com>
2019-06-11 20:55:42 +0200

    timer experiment
    attempt to make timer

--- timer.js
+++ timer.js
@@ -1,12 +1,19 @@
 //Timer Fuction
+function pomodoroTimer () {
+var durationMin = 25;
+var durationSec = 60;
 
-var duration = 25;
-var secDuration = 5;
+while (durationMin > 0) {
+     a = setInterval(
+    if (durationSec > 0) {
+    durationSec -= 1;
+    console.log(durationSec)},100);
+} else {
+    durationMin -= 1;
+    console.log(durationMin);
+}
+}
 
-
-
-
-document.getElementById('minutes').innerHTML= duration;
-document.getElementById('sec').innerHTML= secDuration;
-console.log('done');
-
+// document.getElementById('minutes').innerHTML= durationMin;
+// document.getElementById('sec').innerHTML= durationSec;
+}
