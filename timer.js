ec3fa447f6cf27662d5d67bd99e417b3d77b8a6b
ZealousSloth <49397161+ZealousSloth@users.noreply.github.com>
2019-06-11 20:25:41 +0200

    add html and css
    basic html structure and css styling

---
+++ timer.js
@@ -0,0 +1,12 @@
+//Timer Fuction
+
+var duration = 25;
+var secDuration = 5;
+
+
+
+
+document.getElementById('minutes').innerHTML= duration;
+document.getElementById('sec').innerHTML= secDuration;
+console.log('done');
+
