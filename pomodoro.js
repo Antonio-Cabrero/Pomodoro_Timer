4176720e7be8e89bec7388a706dc3bc4405bd35b
ZealousSloth <49397161+ZealousSloth@users.noreply.github.com>
2019-10-31 18:54:24 +0100

    Redesigned the UI

--- pomodoro.js
+++ pomodoro.js
@@ -40,7 +40,6 @@ function sessionTimer() {
   
   timer = setInterval( () => { // RUNS EACH FUNCTION EVERY SECOND
     
-    pauseBtn(timer);              //  CHECKS FOR PAUSING
     secondsCounter();             //  SUBTRACTS 1 SECOND & CHECKS IF SECONDS IS NOT 0, IF ITS THEN IT SUBSTRACTS 1 FROM MINUTES AND RESETS THE SECONDS 
     printTime();                  //  PRINTS NUMBERS TO DOM
     timerStop(timer);             //  STOPS TIMER IF MINUTES & SECONDS REACHES 0
@@ -63,10 +62,10 @@ function sessionSetUp () {                   // SETS THE TIME OF THE SESSION
 
 function startSession () {
   
-  timerBody.style.backgroundColor = '#399721';  // SETS BACK GROUND TO COLOR FOR 'IN WORK SESSION'
+  timerBody.style.backgroundColor = '#6ED05E';  // SETS BACK GROUND TO COLOR FOR 'IN WORK SESSION'
   sessionSetUp();
   sessionTimer();                               // STARTS TIMER
-
+  working = true;
 } 
 
 function breakSession () { // STARTS A BREAK TIMER
@@ -115,7 +114,7 @@ function timerStop(timer) {
   if (minutes === 0 && seconds === 0) {
     
     clearInterval(timer);                                                 // STOPS INTERVAL
-    timerBody.style.backgroundColor = '#1e7fda';                          // Bg COLOR CHANGE TO BREAK COLOR
+    timerBody.style.backgroundColor = '#5E99D0';                          // Bg COLOR CHANGE TO BREAK COLOR
     workCounter[ workSession ].style.backgroundColor = '#399721';         // ADD 1 TO WORK SESSION TRACKER
     workSession += 1;                                                     // ALSO ADD 1 TO THE VARIABLE WORK COUNTER
     alertDisplay();
@@ -123,18 +122,6 @@ function timerStop(timer) {
 
 }
 
-function pauseBtn(timer){         // CREATES EVENT LISTENER FOR PAUSE BTN
-  pauseInt.addEventListener('click', (e) => {
-      
-    let element = e.target;
-    if (element === pauseInt){    // CHECKS IF PAUSE BUTTON IS CLICKED
-    
-      pause = true;               // CHANGES VALUE OF PAUSE VARIABLE
-      clearInterval(timer);       // STOPS INTERVAL  
-    } 
-  })
-
-}
 
 
 function resetWork() {
@@ -188,29 +175,6 @@ function stopBell() {  // STOPS AND RESETS THE BELL SOUND
   chimeBell.currentTime = 0;
 }
 
-function hideButtons () {
-
-  startInt.disabled = true
-  pauseInt.disabled = true
-  startInt.style.opacity = "0"
-  pauseInt.style.opacity = "0"
-  startInt.style.transform = "translateX(150px)"
-  pauseInt.style.transform = "translateX(-150px)"
-  resetBtn.style.zIndex = "5"
-  resetBtn.innerText = "Ok"
-}
-
-function unhideButtons() {
- 
-    startInt.style.opacity = "1"
-    pauseInt.style.opacity = "1"
-    startInt.style.transform = "translateX(0px)"
-    pauseInt.style.transform = "translateX(0px)"
-    startInt.disabled = false
-    pauseInt.disabled = false
-    resetBtn.style.zIndex = "0"
-    resetBtn.innerText = "Reset"
-}
 
 function validateInput() {
   let reg = /\D/g
@@ -233,23 +197,48 @@ startInt.addEventListener('click', ()=>{
 
   if (pause === false && working === false) {     // CHEKS OF PROGRAM IS NOT ON PAUSE and PREVENTS  TIMER FROM STARTING AGAIN
     startSession();                               // START A NEW WORK SESSION
-    working = true;
+    startInt.innerHTML = `<svg width="32" height="42" viewBox="0 0 32 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 0H13.2174V42H0V0Z" fill="#313131"/>
+                            <path d="M18.7826 0H32V42H18.7826V0Z" fill="#313131"/>
+                          </svg>`
 
-  } else if (pause === true) {  // CHECKS IF PROGRAM IS ON PAUSE
+  } 
+  else  if (working === true && pause === false){    // CHECKS IF PAUSE BUTTON IS CLICKED   
+    pause = true;               // CHANGES VALUE OF PAUSE VARIABLE
+    clearInterval(timer);       // STOPS INTERVAL  
+    startInt.innerHTML = `<svg width="40" height="52" viewBox="-5 0 40 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M36 26L0.750001 51.1147L0.750004 0.885262L36 26Z" fill="#000"/>
+                          </svg>`
+  }
+  
+  else if (working === true && pause === true) {  // CHECKS IF PROGRAM IS ON PAUSE
     pause = false;              // CHANGES PAUSE VALUE (TO UNPAUSE)
     sessionTimer();             // CONTINUES TIMER
+    startInt.innerHTML = `<svg width="32" height="42" viewBox="0 0 32 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M0 0H13.2174V42H0V0Z" fill="#313131"/>
+    <path d="M18.7826 0H32V42H18.7826V0Z" fill="#313131"/>
+    </svg>`
   }
 
 });
 
 resetBtn.addEventListener('click', (e)=> {
-  let element = e.target;
 
-  if (element === resetBtn) {
+  
     clearInterval(timer);
     working = false;
-    timerBody.style.backgroundColor = "#660b0b"
+    pause = false;
+    timerBody.style.backgroundColor = "#D05E5E"
     sessionSetUp();
+  
+  if (resetBtn.innerHTML === "OK") {
+    intervalMin = validateInput();
+    timerField.style.display = "none";
+    timerMin.innerText = `${intervalMin}`;
+    timerMin.style.display = "block";
+    resetBtn.innerHTML = `<svg width="37" height="38" viewBox="0 0 37 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5.75945 11.1315C7.5283 8.50113 10.106 6.51871 13.1023 5.48435L15.4924 12.4081C14.0011 12.9229 12.718 13.9097 11.8376 15.2189C10.9572 16.5281 10.5273 18.0886 10.613 19.664C10.6987 21.2394 11.2954 22.744 12.3127 23.95C13.3299 25.156 14.7125 25.9977 16.2509 26.3477C17.7893 26.6977 19.3999 26.5369 20.8388 25.8897C22.2777 25.2425 23.4665 24.1441 24.2254 22.7608C24.9842 21.3775 25.2717 19.7847 25.0443 18.2234C24.8797 17.0937 24.4521 16.0251 23.8033 15.1004L21.234 18.314L16.7577 4.78024L30.9449 6.16777L28.4701 9.26313C30.5232 11.4397 31.86 14.1988 32.2925 17.1677C32.7493 20.3044 32.1718 23.5045 30.6472 26.2836C29.1227 29.0627 26.7343 31.2694 23.8434 32.5697C20.9526 33.87 17.7169 34.193 14.6261 33.4898C11.5353 32.7867 8.75769 31.0956 6.71389 28.6728C4.67009 26.2499 3.47134 23.227 3.29912 20.0619C3.12691 16.8968 3.9906 13.7618 5.75945 11.1315Z" fill="#313131"/>
+                          </svg>`
   }
 })
 
@@ -281,16 +270,10 @@ timerBody.addEventListener('click', (e)=> {
     if (e.target === timerMin && working === false) {
       timerMin.style.display = "none"
       timerField.style.display = "inline-block"
-      hideButtons()
+      resetBtn.innerHTML = "OK"
     }
+
     
-    if (e.target.innerText === "Ok") {
-        intervalMin = validateInput();
-        timerField.style.display = "none";
-        timerMin.innerText = `${intervalMin}`;
-        timerMin.style.display = "block";
-        unhideButtons()
-    }
 })
 
  
